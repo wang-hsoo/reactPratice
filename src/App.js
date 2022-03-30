60345d41679ae608141814297abc41c777800eef
wang-hsoo <dhkd4873@naver.com>
2022-03-30 11:46:58 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import style from "./App.module.css";
+
+
 
 function App() {
+  const [lone, setlon] = useState();
+  const [lati, setlat] = useState();
+  
+  const new_script = src => { 
+    return new Promise((resolve, reject) => { 
+      const script = document.createElement('script'); 
+      script.src = src; 
+      script.addEventListener('load', () => { 
+        resolve(); 
+      }); 
+      script.addEventListener('error', e => { 
+        reject(e); 
+      }); 
+      document.head.appendChild(script); 
+    }); 
+  };
+
+  useEffect(() => { 
+    navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+    //카카오맵 스크립트 읽어오기
+    const my_script = new_script('https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=8894cb8f3eedf8df828b4dac6c91c873');
+    
+    //스크립트 읽기 완료 후 카카오맵 설정
+    my_script.then(() => { 
+      console.log(lati,lone);  
+      const kakao = window['kakao']; 
+      kakao.maps.load(() => {
+        const mapContainer = document.getElementById('map');
+        const options = { 
+          center: new kakao.maps.LatLng(lati, lone), //좌표설정
+          level: 3 
+          
+        }; 
+        const map = new kakao.maps.Map(mapContainer, options); //맵생성
+        //마커설정
+        const markerPosition = new kakao.maps.LatLng(lati,lone); 
+        const marker = new kakao.maps.Marker({ 
+          position: markerPosition
+        }); 
+        marker.setMap(map); 
+      });   
+    });
+  }, [{lati, lone}]);
+
+
+  const onGeoOk = (position) => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    
+    setlon(lon);
+    setlat(lat);
+  }
+
+  const onGeoError = () => {
+    
+  }
+ 
+    
+  
+
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id = "map"className={style.map} ></div>
     </div>
   );
 }
